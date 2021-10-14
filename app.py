08095d55892b5b426c41693aa6fe0c87cd5e114e
DANIEL <daniel_cornwall@hotmail.com>
2021-10-14 20:14:43 +0000

    connect flask to github

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
-from flask import (Flask, flash, 
-render_template, redirect, request, 
-session, url_for)
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
@@ -20,13 +20,11 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_tasks")
 def get_tasks():
-    tasks = mongo.db.taks.find()
-    return render_template(("template_name_or_list"))
-    return "Hello World ... again!"
+    tasks = mongo.db.tasks.find()
+    return render_template("tasks.html", tasks=tasks)
 
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
-
