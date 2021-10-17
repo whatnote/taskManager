d3af6182cfb9027529037fc40e14e039e3f6e15f
DANIEL <daniel_cornwall@hotmail.com>
2021-10-17 22:03:57 +0000

    regatistration function

--- app.py
+++ app.py
@@ -25,8 +25,26 @@ def get_tasks():
     return render_template("tasks.html", tasks=tasks)
 
 
-@app.route("/register", methods=["get", "post"])
+@app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        #put the new user into 'seddion' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registraion successful")
     return render_template("register.html")
 
 
